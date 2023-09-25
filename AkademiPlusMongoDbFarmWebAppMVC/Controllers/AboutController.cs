8d91de3bdef6593577ebc4e09afbf9bc3c65b44e
Yakup Almatepe <almatepe.yakup@gmail.com>
2023-09-26 02:09:28 +0300

    Add project files.

---
+++ AkademiPlusMongoDbFarmWebAppMVC/Controllers/AboutController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiPlusMongoDbFarmWebAppMVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OurInfoPartial()
+        {
+            return PartialView();
+        }
+        public IActionResult WhatWeDoPartial()
+        {
+            return View();
+        }
+        public IActionResult RootWithNaturePartial()
+        {
+            return View();
+        }
+        public IActionResult WhyUsPartial()
+        {
+            return View();
+        }
+        public IActionResult HappyClientsPartial()
+        {
+            return View();
+        }
+        public IActionResult FooterPartial()
+        {
+            return View();
+        }
+    }
+}
---
+++ AkademiPlusMongoDbFarmWebAppMVC/Models/About.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace AkademiPlusMongoDbFarmWebAppMVC.Models
+{
+    public class About
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string AboutId { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
